4d31e2195a948b452925178ae38e98a007d6eb99
Leonid Goryaynov <lbglbg999@gmail.com>
2020-01-05 19:06:57 +0700

    Дабавлен тест

--- MapGeneratorTests/ForestLayerBuilderTest.cs
+++ MapGeneratorTests/ForestLayerBuilderTest.cs
@@ -9,10 +9,26 @@ namespace MapGeneratorTests
     public class ForestLayerBuilderTest
     {
         [TestMethod]
-        public void BuildLayer_AddSomeForestCells_AnyForestCellsWillBeOnMap()
+        public void BuildLayer_AddSomeForestCellsWithMap10By10_AnyForestCellsWillBeOnMap()
         {
             //arrange
             var map = new MapGenerator.Map(10, 10);
+            var builder = new ForestLayerBuilder();
+
+            //act
+
+            map = builder.BuildLayer(ref map, 1, 10, 20);
+
+            //assert
+
+            Debug.Assert(map.Cells.Any(x => x is MapGenerator.Cells.ForestCell));
+        }
+
+        [TestMethod]
+        public void BuildLayer_AddSomeForestCellsWithMap20By20_AnyForestCellsWillBeOnMap()
+        {
+            //arrange
+            var map = new MapGenerator.Map(20, 20);
             var builder = new ForestLayerBuilder();
 
             //act
